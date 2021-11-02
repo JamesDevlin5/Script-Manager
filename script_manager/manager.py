1e00d992a77dcff4469d088c11ba2a1ac9fcdc9f
James Devlin <devlin.ja@northeastern.edu>
2021-11-02 00:40:02 -0400

    Added some colors
    Added a color object, for manipulating ANSI colors. Added a little bit of red to one
    component, and the padding is completely thrown off. To be fixed.

--- script_manager/manager.py
+++ script_manager/manager.py
@@ -4,6 +4,68 @@ from pathlib import Path
 import typing
 
 
+class Color:
+    # Map of the color's names to their respective index (internally)
+    # Lowercase all color names to avoid case-sensitive matching
+    ColorMap = [
+        name.lower()
+        for name in [
+            "Black",
+            "Red",
+            "Green",
+            "Yellow",
+            "Blue",
+            "Purple",
+            "Teal",
+            "White",
+        ]
+    ]
+    # And the code to reset all these style changes
+    Reset = 0
+
+    @staticmethod
+    def _encode_num(color_code: int) -> str:
+        # <Esc>[<Num>m ...string message... <Esc>[0m
+        return "\x1B[" + str(color_code) + "m"
+
+    @classmethod
+    def _map_lookup(cls, name: str, code_map: list[int]) -> str:
+        """Utility function to print a color, additionally given a map to translate the colors into codes"""
+        # Gets the integer-index that is internally associated with the color name argument
+        color_idx = cls.ColorMap.index(name.lower())
+        color_code = code_map[:][color_idx]
+        return cls._encode_num(color_code)
+
+    @classmethod
+    def fg(cls, name: str) -> int:
+        """ANSI foreground color code"""
+        return cls._map_lookup(name, [30, 31, 32, 33, 34, 35, 36, 37])
+
+    @classmethod
+    def bg(cls, name: str) -> int:
+        """ANSI background color code"""
+        return cls._map_lookup(name, [40, 41, 42, 43, 44, 45, 46, 47])
+
+    @classmethod
+    def reset(cls) -> str:
+        """Gets the code sequence to reset all attribute stylings"""
+        return cls._encode_num(cls.Reset)
+
+
+class BrightColor(Color):
+    """ANSI bright/bold/... form of an escaped color code"""
+
+    @classmethod
+    def fg(cls, name: str) -> int:
+        """ANSI bright foreground color code"""
+        return cls._map_lookup(name, [90, 91, 92, 93, 94, 95, 96, 97])
+
+    @classmethod
+    def bg(cls, name: str) -> int:
+        """ANSI bright background color code"""
+        return cls._map_lookup(name, [100, 101, 102, 103, 104, 105, 106, 107])
+
+
 class ScriptFile:
     def __init__(self, path: Path):
         self._path = path
@@ -24,7 +86,7 @@ class ScriptFile:
                     return line.split(" ")[1].rstrip()
         except UnicodeDecodeError:
             pass
-        return "??"
+        return BrightColor.fg("Red") + "??" + Color.reset()
 
     def num_lines(self) -> typing.Tuple[int, int]:
         line_count = 0
